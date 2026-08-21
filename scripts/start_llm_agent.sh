d00e83b9a281ffc9a69e88c44ab7c48c17929ec3
robot-agent <robot@localhost>
2026-08-21 07:16:22 +0000

    fix: forward GEMINI_API_KEY through start_llm_agent.sh env -i (was dropping key, caused BLOCKED)

--- scripts/start_llm_agent.sh
+++ scripts/start_llm_agent.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-exec env -i PATH=/usr/bin:/bin:/opt/ros/jazzy/bin HOME=/home/ubuntu LANG=C.UTF-8 \
+exec env -i PATH=/usr/bin:/bin:/opt/ros/jazzy/bin HOME=/home/ubuntu LANG=C.UTF-8 GEMINI_API_KEY="$GEMINI_API_KEY" \
   bash -c 'source /opt/ros/jazzy/setup.bash && source /home/ubuntu/humanoid_sim_ws/install/setup.bash && exec ros2 run h1_llm_agent agent_node --ros-args --params-file /home/ubuntu/humanoid_sim_ws/src/h1_llm_agent/config/gemini.yaml'
